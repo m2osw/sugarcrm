c0426cd1a4784a64464c5e2aaa410b0ad39ae54d
AlexisWilke <alexis@m2osw.com>
2017-04-27 01:11:48 -0700

    Fixed #1 by removing the "?action=ajaxui#ajaxUILoc=", this is a hack, but things work a lot better.

--- include/utils/mvc_utils.php
+++ include/utils/mvc_utils.php
@@ -2,31 +2,31 @@
 /*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
  * Free Software Foundation with the addition of the following permission added
  * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
  * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
  * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with
  * this program; if not, see http://www.gnu.org/licenses or write to the Free
  * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA.
- * 
+ *
  * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
  * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU Affero General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo. If the display of the logo is not reasonably feasible for
@@ -40,7 +40,9 @@ function loadParentView($type)
     if(file_exists('custom/include/MVC/View/views/view.'.$type.'.php'))
     {
         require_once('custom/include/MVC/View/views/view.'.$type.'.php');
-    } else if(file_exists('include/MVC/View/views/view.'.$type.'.php')) {
+    }
+    elseif(file_exists('include/MVC/View/views/view.'.$type.'.php'))
+    {
         require_once('include/MVC/View/views/view.'.$type.'.php');
     }
 }
@@ -48,7 +50,7 @@ function loadParentView($type)
 
 function getPrintLink()
 {
-    if (isset($_REQUEST['action']) && $_REQUEST['action'] == "ajaxui")
+    if(isset($_REQUEST['action']) && $_REQUEST['action'] == "ajaxui")
     {
         return "javascript:SUGAR.ajaxUI.print();";
     }
@@ -57,7 +59,8 @@ function getPrintLink()
 }
 
 
-function ajaxBannedModules(){
+function ajaxBannedModules()
+{
     $bannedModules = array(
         'Calendar',
         'Emails',
@@ -101,15 +104,18 @@ function ajaxBannedModules(){
         'OAuthKeys'
     );
 
-    if(!empty($GLOBALS['sugar_config']['addAjaxBannedModules'])){
+    if(!empty($GLOBALS['sugar_config']['addAjaxBannedModules']))
+    {
         $bannedModules = array_merge($bannedModules, $GLOBALS['sugar_config']['addAjaxBannedModules']);
     }
-    if(!empty($GLOBALS['sugar_config']['overrideAjaxBannedModules'])){
+    if(!empty($GLOBALS['sugar_config']['overrideAjaxBannedModules']))
+    {
         $bannedModules = $GLOBALS['sugar_config']['overrideAjaxBannedModules'];
     }
 
     return $bannedModules;
 }
+
 
 function ajaxLink($url)
 {
@@ -120,20 +126,28 @@ function ajaxLink($url)
     preg_match('/module=([^&]*)/i', $url, $match);
     preg_match('/^javascript/i', $url, $javascriptMatch);
 
-    if(!empty($sugar_config['disableAjaxUI'])){
+    if(!empty($sugar_config['disableAjaxUI']))
+    {
         return $url;
     }
-    else if(isset($match[1]) && in_array($match[1], ajaxBannedModules())){
+    elseif(isset($match[1]) && in_array($match[1], ajaxBannedModules()))
+    {
         return $url;
     }
     //Don't modify javascript calls.
-    else if (isset($javascriptMatch[0])) {
-    	return $url;
+    elseif(isset($javascriptMatch[0]))
+    {
+        return $url;
     }
     else
     {
-        return "?action=ajaxui#ajaxUILoc=" . urlencode($url);
+    // At this time the AJAX stuff is not working right because the
+    // necessary JavaScript is not loaded correct so I removed that
+    // functionality. It's about the speed anyway since in most
+    // cases the entire page is loaded through this interface.
+        return $url;
+        //return "?action=ajaxui#ajaxUILoc=" . urlencode($url);
     }
 }
 
-?>
+// vim: ts=4 sw=4 et
